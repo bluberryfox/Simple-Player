4978985d73dfcf7baeae7ce876c44e994bb84f61
blueberryfox <nastja1905@mail.ru>
2018-02-15 01:20:06 +0500

    я понял, что дурак и что надо было по mvvm

---
+++ Player/Player/ViewModel/MainWindowController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player.PlayerApplication
+{
+    class MainWindowController
+    {
+        public static List<string> Paths;
+        public static List<string> SelectFiles()
+        {
+            List<string> files = new List<string>();
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Multiselect = true,
+                Filter = "Media files (*.mp3;*.mpg;*.mpeg)|*.mp3;*.mpg;*.mpeg|All files (*.*)|*.*"
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Paths = openFileDialog.FileNames.ToList();
+                foreach (string filename in openFileDialog.SafeFileNames)
+                    files.Add(System.IO.Path.GetFileNameWithoutExtension(filename));
+            }
+            return files;
+
+        }
+    }
+}
