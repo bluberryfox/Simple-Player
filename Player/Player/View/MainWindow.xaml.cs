6acb36c895a2656ade2a224ce6ad725654a130b4
blueberryfox <nastja1905@mail.ru>
2018-02-16 03:54:52 +0500

    fixed buttons methods

--- Player/Player/View/MainWindow.xaml.cs
+++ Player/Player/View/MainWindow.xaml.cs
@@ -37,11 +37,11 @@ namespace Player.View
             {
                 Interval = TimeSpan.FromSeconds(1)
             };
-            timer.Tick += Timer_Tick;
+            timer.Tick += TimerTick;
             timer.Start();
             controller = new MainWindowController();
         }
-        private void Timer_Tick(object sender, EventArgs e)
+        private void TimerTick(object sender, EventArgs e)
         {
             if ((media.Source != null) && (media.NaturalDuration.HasTimeSpan) && (!userIsDraggingSlider))
             {
@@ -50,33 +50,48 @@ namespace Player.View
                 progress.Value = media.Position.TotalSeconds;
             }
         }
-        private void Progress_DragStarted(object sender, DragStartedEventArgs e)
+
+        private void ProgressDragStarted(object sender, DragStartedEventArgs e)
         {
             userIsDraggingSlider = true;
         }
-        private void Progress_DragCompleted(object sender, DragCompletedEventArgs e)
+        private void ProgressDragCompleted(object sender, DragCompletedEventArgs e)
         {
             userIsDraggingSlider = false;
             media.Position = TimeSpan.FromSeconds(progress.Value);
         }
 
-
-        private void Play_Click(object sender, RoutedEventArgs e)
+        private void PlayClick(object sender, RoutedEventArgs e)
         {
             media.Play();
         }
+        private void PauseClick(object sender, RoutedEventArgs e)
+        {
+            media.Pause();
+        }
 
-        private void PreviosSong_Click(object sender, RoutedEventArgs e)
+        private void AddClick(object sender, RoutedEventArgs e)
+        {
+            media.Stop();
+            playlist.ItemsSource = controller.FindAllFiles();
+        }
+
+        private void StopClick(object sender, RoutedEventArgs e)
+        {
+            media.Stop();
+        }
+
+        private void PreviosSongClick(object sender, RoutedEventArgs e)
         {
             ChangeSong(playlist.SelectedIndex, x => x - 1);
         }
 
-        private void NextSong_Click(object sender, RoutedEventArgs e)
+        private void NextSongClick(object sender, RoutedEventArgs e)
         {
-            ChangeSong(playlist.SelectedIndex, x=>x+1);
+            ChangeSong(playlist.SelectedIndex, x => x + 1);
         }
 
-        private void Playlist_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void PlaylistSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             string path = controller.GetPathToFile(playlist.SelectedIndex);
             var temp = controller.GetSongInfo(path);
@@ -86,28 +101,13 @@ namespace Player.View
             PlayMedia(path);
         }
 
-        private void Pause_Click(object sender, RoutedEventArgs e)
-        {
-            media.Pause();
-        }
-
-        private void Add_Click(object sender, RoutedEventArgs e)
-        {
-            media.Stop();
-            playlist.ItemsSource = controller.FindAllFiles();
-        }
-
-        private void Stop_Click(object sender, RoutedEventArgs e)
-        {
-            media.Stop();
-        }
         private void PlayMedia(string path)
         {
             media.Source = new Uri(path);
             media.Play();
         }
 
-        private void Media_MediaEnded(object sender, RoutedEventArgs e)
+        private void MediaMediaEnded(object sender, RoutedEventArgs e)
         {
             media.Stop();
 
